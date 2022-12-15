081b312383c6b92d26881a01387554cc75e6f10c
james <50460460+alexacallmebaka@users.noreply.github.com>
2022-12-15 16:29:59 -0600

    main didnt get in last commit

--- main.hs
+++ main.hs
@@ -1,40 +1,7 @@
 import System.IO
 import Control.Applicative hiding ((<|>),many)
 import Text.Parsec
-
---TODO:
---Image
---link
---embedded html
---code (block and inline)
---templating
---bold, italics
---footnotes?
-
-
---TEXT SHOULD BE A MONAD???
-
-data Element = Header | SubHeader | Paragraph deriving (Eq)
-data WebElem  =  WebElem {element :: Element, content :: [Text]} deriving (Eq)
-data Text = Text {style :: TextStyle, words :: String} deriving (Eq)
-data TextStyle = Bold | Italic | Plain deriving (Eq)
-
-instance Show Text where
-     show (Text Bold text) = "<b>" ++ text ++ "</b>"
-     show (Text Italic text) = "<i>" ++ text ++ "</i>"
-     show (Text Plain text) = text
-
-instance Show WebElem where
-     show (WebElem Header text) = htmlify text "<h1>" "</h1>"
-     show (WebElem SubHeader text) = htmlify text "<h2>" "</h2>"
-     show (WebElem Paragraph text) = htmlify text "<p>" "</p>"
-
---I would like this to work with styling text too, the problem is that that will just be type Text not [Text] I think.
-htmlify :: [Text] -> String -> String -> String
-htmlify text open close = open ++ (foldl (\acc x -> acc ++ (show x)) "" text) ++ close
-
---type Paragraph = [Text]
---type Website = [WebElem]
+import Web
 
 file = endBy line eol
 
