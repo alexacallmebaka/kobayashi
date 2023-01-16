f8177efe92ef3a79ae69f7ac4c109e2de91ab5f1
james <50460460+alexacallmebaka@users.noreply.github.com>
2023-01-16 17:30:59 -0600

    cleaned up buildsite

--- main.hs
+++ main.hs
@@ -16,9 +16,8 @@ dispatch :: Map.Map String (Map.Map String String -> [String] -> IO ()) --{{{1
 dispatch = Map.fromList [("build", \x y-> buildPage x $ head y)]
 --1}}}
 
---flag default values.
-defaults :: Map.Map String String --{{{1
-defaults = Map.fromList [("-odir",".")]
+options :: [String] --{{{1
+options = ["-odir"]
 --1}}}
 
 --help command.
@@ -35,50 +34,51 @@ help = mapM_ putStrLn [ "Usage: kobayashi [options] <command> \n"
 
 --arg handlers.
 
---process flags 
+--process flags.
 processFlags :: Map.Map String String -> [String] -> (Map.Map String String, [String]) --{{{1
-processFlags flags x@(flg:arg:xs) = case Map.lookup flg flags of
-                                      Nothing -> (flags, x)
-                                      Just _ -> processFlags newFlags xs
+processFlags flags x@(flg:arg:xs) = case flg `elem` options of
+                                      False -> (flags, x)
+                                      True -> processFlags newFlags xs
                                         where newFlags = Map.insert flg arg flags               
 processFlags flags x = (flags, x)
 --1}}} 
 
-
+--process command.
 processCMD :: Map.Map String String -> [String] -> IO () --{{{1
 processCMD _  [] = putStrLn "No commands, nothing to do..."
 processCMD _ ["help"] = help
 processCMD opts (cmd:args) = case Map.lookup cmd dispatch of
                               Just action -> action opts args
-                              Nothing -> putStrLn $ printf "[ERROR] halting due to invalid command: %s\n" cmd
+                              Nothing -> printf "[ERROR] halting due to invalid command: %s\n" cmd
 --1}}}
 
 --lex, parse, htmlify, then print html or errors.
 buildPage :: Map.Map String String -> String -> IO () --{{{1
 buildPage flags source = do 
-              printf "Building %s...\n" source
-              input <- readFile source
-              case tokenize source input of
-                Left err -> print err
-                Right tokens -> case parse source tokens of
-                  Left err -> print err
-                  Right doc -> case Map.lookup "-odir" flags of
-                    Just dir -> do
-                      createDirectoryIfMissing True dir
-                      let content = concat $ map ((++ "\n") . htmlify) doc
-                          page = "<!DOCTYPE HTML>\n<html>\n<body>\n" ++  content ++ "</body>\n</html>\n"
-                          outfile = dir </> source -<.> ".html"
-                       in writeFile outfile page
-                    Nothing -> putStrLn "[ERROR] no output directory specified. Please run \"kobayashi help\" for options."
---1}}}
-
---entrypoint + timing
-main = do --{{{1
   start <- getCPUTime
-  args <- getArgs
-  let (opts, cmd) = processFlags defaults args
-  processCMD opts cmd
+  printf "Building %s...\n" source
+  input <- readFile source
+  case tokenize source input >>= parse source of
+    Left err -> print err
+    Right doc -> do
+      dir <- return $ case Map.lookup "-odir" flags of
+                        Just custom -> custom
+                        Nothing -> "."
+      createDirectoryIfMissing True dir
+      let content = concat $ map ((++ "\n") . htmlify) doc
+          page = "<!DOCTYPE HTML>\n<html>\n<body>\n" ++  content ++ "</body>\n</html>\n"
+          outfile = dir </> source -<.> ".html"
+       in writeFile outfile page
   end <- getCPUTime
   let time = fromIntegral (end-start) / (10^12)
    in printf "Finished in %0.4f sec.\n" (time :: Double)
+--1}}}
+
+--todo: throw error if args after cmd
+
+--entrypoint
+main = do --{{{1
+  args <- getArgs
+  let (opts, cmd) = processFlags Map.empty args
+  processCMD opts cmd
 --1}}}
