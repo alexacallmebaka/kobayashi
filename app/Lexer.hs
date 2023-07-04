577428f615cd7125faa6ec3b8339c091001ef3af
james <50460460+alexacallmebaka@users.noreply.github.com>
2023-07-04 13:17:18 -0500

    added header and subheader

--- app/Lexer.hs
+++ app/Lexer.hs
@@ -8,11 +8,25 @@ import qualified Token as KBYToken
 
 import qualified Data.Text as T
 import Data.Void
+
 import Control.Applicative hiding (some, many)
+
+--import qualified Text.Megaparsec.Char.Lexer as L
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
 type Parser = Parsec Void T.Text
+
+--spaces :: Parser ()
+--spaces = L.space eol empty empty
+
+-- headers {{{1
+headerPrefix :: Parser ()
+headerPrefix = () <$ char '@' 
+
+header :: Parser KBYToken.Token
+header = headerPrefix *> option KBYToken.BeginHeader (KBYToken.BeginSubheader <$ headerPrefix)
+-- 1}}}
 
 line :: Parser [KBYToken.Token]
 line = many $ choice
@@ -22,10 +36,10 @@ line = many $ choice
 
 -- inline stuff {{{1
 bold :: Parser KBYToken.Token
-bold = KBYToken.Bold <$ char '*'
+bold = KBYToken.Bold <$ char '*' <?> "bold"
 
 italic :: Parser KBYToken.Token
-italic = KBYToken.Italic <$ char '/'
+italic = KBYToken.Italic <$ char '/' <?> "italic"
 
 textChar :: Parser KBYToken.Token
 textChar = KBYToken.TextChar . T.singleton <$> (optional (char '\\') *> printChar)
