067e4c5ba4b828617f7811ad298d05b04d7a47fc
james <50460460+alexacallmebaka@users.noreply.github.com>
2023-07-16 20:48:20 -0500

    building an entire page

--- app/Builder.hs
+++ app/Builder.hs
@@ -1,24 +1,46 @@
 --build website from DocuElem.
 module Builder
-    ( toHTML
+    ( buildPage
     ) where
+
+import qualified Data.Text as T
+import Data.List
+import Data.Void
+
+import Text.Megaparsec.Error
 
 import HTML
 import KBYDoc
+import KBYToken
 import Lexer
 import Parser
-import Data.List
 
---htmlify list of DocuElem then print html or errors.
+type BuildError = String
+type SourceName = String
+
+--htmlify internal Document.
 toHTML :: Document -> String --{{{1
 toHTML doc = "<!DOCTYPE HTML>\n<html>\n<body>\n" ++  content ++ "</body>\n</html>\n"
     where content = concatMap ((++ "\n") . htmlify) doc
 --1}}}
 
+--lex a file and return a stream of tokens or an error as a string.
+lexFile :: SourceName -> T.Text -> Either BuildError KBYStream
+lexFile source input = case tokenize source input of
+        Left err -> Left $ errorBundlePretty err
+        Right tokens -> Right tokens
+
+--parse a file and return a stream of tokens or an error as a string.
+parseFile :: SourceName -> KBYStream -> Either BuildError Document
+parseFile source input = case parseTokens source input of
+        Left err -> Left $ errorBundlePretty err
+        Right doc -> Right doc
+
+
 -- kby => html.
---buildPage :: SourceName -> String -> Either ParseErrorBundle String --{{{1
---buildPage source input = 
---    case tokenize source input >>= parse source of
---        Left err -> Left err
---        Right doc -> Right $ toHTML doc
+buildPage :: SourceName -> T.Text -> Either BuildError String --{{{1
+buildPage source input = 
+    case (lexFile source input) >>= (parseFile source) of
+        Right doc -> Right $ toHTML doc
+        Left x -> Left x
 --1}}}
