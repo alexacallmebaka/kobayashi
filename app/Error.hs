2b5e6396ae2e75937334df46567a9c32eb4b17f5
james <50460460+alexacallmebaka@users.noreply.github.com>
2024-01-09 17:25:10 -0600

    document and style Error.hs

--- app/Error.hs
+++ app/Error.hs
@@ -1,11 +1,17 @@
+--errors and related functions.
+
 module Error
-  ( BuildError(..)
+  ( 
+    BuildError(..)
   , ErrorMsg
+
   , printErrors
   ) where
 
-import Data.Text
+import Data.Text (Text, append)
+
 import qualified Data.Text.IO as TIO
+
 
 type ErrorMsg = Text
 
@@ -17,8 +23,8 @@ printErrors :: [BuildError] -> IO ()
 printErrors [] = return ()
 printErrors (err:errs) = do
   let errType = case err of  
-               LexError _ -> "[LEXICAL ERROR]"
-               ParseError _ -> "[PARSE ERROR]"
+                  LexError _ -> "[LEXICAL ERROR]"
+                  ParseError _ -> "[PARSE ERROR]"
   let errMsg = errType `append` " " `append` unError err
   TIO.putStrLn errMsg 
   printErrors errs
